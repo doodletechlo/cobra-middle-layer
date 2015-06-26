9af694aafb156c4ab956d20472ea7ae2b062b268
Esteban Leon <eleon182@gmail.com>
2015-06-26 08:52:50 -0700

    getmemebertoken setup

--- api/login/index.js
+++ api/login/index.js
@@ -13,24 +13,36 @@ module.exports = {
 
 function getMemberToken(params) {
     var deferred = q.defer();
-    debug('authentication.js', params);
-    if (params.username == 'testuser' && params.password == 'password') {
-        deferred.resolve({
-            token: 'NzdlNzJkMWQtZWNlZS00MDk1LTk2OGEtMGI0MGZlZjE0NjNl.R1pebh06jMfupcMaluWA6eCjqQ0GfiO9hRbYi+3oLIa2A0RUbooUojWo+XqhqGuMONjQaBBp4X41qjqk9Ueqzy4rr1jb8iH4f3kR++kCU5r6lXsPd2hSh8EtwFclXUyh1pBQAqPCvTdhe+7kVYMzqqYx9SX6b/TMc4KbnA8xtMX5dOSk9oT3vAvCwM3CRGruA9rme/VgRii/QDogLJlcImDu8V2ubXcwnNPtK/9wOMFGPXy1b9SxzD6vLVhUWP662s3sJfjNgM83+2jinqwg2iQMUHXWGStx5NC2WX4DtCLEI/4Cijs0Q16jACh9zGYUUIWoVGo7p/nEtehTD7U1pQ=='
-        });
-    } else {
+    if (!params.password || !params.username) {
         deferred.reject({
             code: 'denied',
             description: 'Invalid credentials'
         });
+    } else {
+        var token = false;
+        common.db.scan(table).then(function(data) {
+            data.forEach(function(val) {
+                if (bcrypt.compareSync(params.password, val.password) && params.username === val.username) {
+                    token = val.customerId;
+                }
+            });
+            if (token) {
+                deferred.resolve({
+                    token: token
+                });
+            } else {
+                deferred.reject({
+                    code: 'denied',
+                    description: 'Invalid credentials'
+                });
+            }
+        }, function(err) {
+            debug('error', err);
+            deferred.reject(err);
+        });
     }
     return deferred.promise;
 }
-
-createUser({
-    username: 'testuser',
-    password: 'password'
-});
 
 function createUser(params) {
 
