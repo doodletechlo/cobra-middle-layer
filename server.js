8c4e0488cc9e76178ba5b168471fcf797ab1100b
Esteban Leon <eleon182@gmail.com>
2015-07-02 18:17:07 -0700

    remove jade

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ var mock = require('./mock');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, ''));
-app.set('view engine', 'jade');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
